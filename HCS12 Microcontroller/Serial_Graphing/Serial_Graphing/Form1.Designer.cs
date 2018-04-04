dce7f1a0cb211036e486397ef18e52d5614a334d
VixTips <38059988+VixTips@users.noreply.github.com>
2018-04-04 15:14:06 -0400

    C# code to proccess the serial communications

---
+++ HCS12 Microcontroller/Serial_Graphing/Serial_Graphing/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Serial_Graphing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MC_Analog = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MC_Analog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Byte Length";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect to port";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1524, 912);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1516, 879);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.MC_Analog);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1516, 879);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(567, 34);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(434, 52);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Input Pulse Graph";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 327);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(156, 308);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Analog Input (Voltage)";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(641, 793);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(296, 36);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Time (10ms)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(98, 828);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // MC_Analog
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.MC_Analog.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.MC_Analog.Legends.Add(legend1);
+            this.MC_Analog.Location = new System.Drawing.Point(83, 34);
+            this.MC_Analog.Name = "MC_Analog";
+            this.MC_Analog.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "Pulse";
+            this.MC_Analog.Series.Add(series1);
+            this.MC_Analog.Size = new System.Drawing.Size(1464, 756);
+            this.MC_Analog.TabIndex = 3;
+            this.MC_Analog.Text = "chart1";
+            title1.Name = "Pulse Sensor Readings";
+            this.MC_Analog.Titles.Add(title1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(49, 963);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(155, 24);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Graph in Voltage";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1678, 1050);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.tabControl1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MC_Analog)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart MC_Analog;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ HCS12 Microcontroller/Serial_Graphing/Serial_Graphing/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+using System.Threading;
+
+
+//TRY THE STRING BUILDER!!!
+
+
+namespace Serial_Graphing
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SerialPort myPort;
+
+        int xCurr=0;
+        char[] rawCharInput = new char[12];
+        int[] rawInput = new int[5000];
+
+        //timer variables..
+        int total;
+        int totalBeats;
+        int sumofInput;
+        //random stupid stuff;
+        int byteLength;
+
+        StringBuilder sb = new System.Text.StringBuilder();
+
+        string rawString = "";
+
+        Queue<double> myList = new Queue<double>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myPort = new SerialPort("COM4", 38400, Parity.None, 8);
+            myPort.Encoding = System.Text.Encoding.ASCII;
+
+            myPort.Open();    
+
+            var thread1 = new Thread(commThread); 
+            thread1.Start();
+
+            button1.Enabled = false;
+        }
+
+        void commThread()
+        {
+            Invoke(new Action(() => MC_Analog.ChartAreas[0].AxisY.Minimum = 1.58));
+            Invoke(new Action(() => MC_Analog.ChartAreas[0].AxisY.Maximum = 1.73));
+            Invoke(new Action(() => label1.Text = "START"));
+
+            while (true)
+            {
+                while (myPort.BytesToRead >= 1)
+                {
+                    Invoke(new Action(() => label1.Text = myPort.BytesToRead.ToString()));
+
+                    sb.Clear();
+                    byteLength = myPort.BytesToRead;
+                    char[] rawChar = new char[byteLength];
+                    myPort.Read(rawChar, 0, byteLength);
+
+                    sb.Append(rawChar);
+                    rawString = rawString + sb.ToString();
+
+
+                    while (rawString.IndexOf(',') >= 0)
+                    {
+                        int divisorIndex = rawString.IndexOf(',');
+                        string val = rawString.Substring(0, divisorIndex);
+                        if (val.Length > 0)
+                        {
+                            if (Int32.Parse(val) < 200)
+                            {
+                                totalBeats = Int32.Parse(val);
+                            }
+                            else
+                            {
+                                if (checkBox1.Checked == true)
+                                {
+                                    myList.Enqueue((Double.Parse(val) / 4096) * 5);
+                                }
+                                else
+                                {
+                                    myList.Enqueue(Double.Parse(val));
+                                }                             
+                              }
+                        }
+                        rawString = rawString.Substring(divisorIndex + 1);
+                    }
+
+                    xCurr++;
+               
+                    if (xCurr > 5)
+                    {
+                        Invoke(new Action(() => MC_Analog.Series["Pulse"].Points.SuspendUpdates()));
+
+                        for (int i = 0; i< myList.Count; i++)
+                        {
+                            Invoke(new Action(() => MC_Analog.Series["Pulse"].Points.AddY(myList.Dequeue())));
+                            total++;
+                        }
+                        myList.Clear();
+
+                        if (total > 500)
+                        {
+                            Invoke(new Action(() => MC_Analog.ChartAreas[0].AxisX.Minimum = total-500));
+                            Invoke(new Action(() => MC_Analog.ChartAreas[0].AxisX.Maximum= total));
+                        }                        
+                        Invoke(new Action(() => MC_Analog.Series["Pulse"].Points.ResumeUpdates()));
+                        Invoke(new Action(() => label4.Text = "BPM: " + totalBeats.ToString()));
+                        xCurr = 0;
+                    }                   
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                Invoke(new Action(() => MC_Analog.ChartAreas[0].AxisY.Minimum = 1.58));
+                Invoke(new Action(() => MC_Analog.ChartAreas[0].AxisY.Maximum = 1.73));
+                Invoke(new Action(() => label7.Text = "Analog Input (Voltage)"));
+            }
+            else
+            {
+                Invoke(new Action(() => MC_Analog.ChartAreas[0].AxisY.Minimum = 1300));
+                Invoke(new Action(() => MC_Analog.ChartAreas[0].AxisY.Maximum = 1400));
+                Invoke(new Action(() => label7.Text = "Analog Input (Bits)"));
+            }
+        }
+
+    }
+}
