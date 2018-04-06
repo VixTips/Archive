361221dfc0e347246f17fc4dbbe6c9ad4a2fa139
VixTips <vicskv@gmail.com>
2018-04-06 10:00:22 -0400

    ..

--- java/JavaApplication1.java
+++ java/JavaApplication1.java
@@ -1,11 +1,14 @@
 //author vic
+// 2SI4 Lab #5
+// April 6, 2018
+
 public class JavaApplication1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+
       testClass mytest = new testClass();
 
       mytest.test();
